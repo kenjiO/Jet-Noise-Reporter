825c37031572e69f1c2080a3e3a407251d14cda9
Kenji Okamoto <kenjio@users.noreply.github.com>
2016-04-08 16:44:59 -0700

    Model classes that reflect the database schema

---
+++ app/src/main/java/edu/westga/jetnoisereporter/Model/IncidentReport.java
@@ -0,0 +1,11 @@
+package edu.westga.jetnoisereporter.Model;
+
+
+import java.util.Date;
+
+public class IncidentReport {
+    private int reportId;
+    private Date date;
+    private User user;
+    
+}
---
+++ app/src/main/java/edu/westga/jetnoisereporter/Model/User.java
@@ -0,0 +1,15 @@
+package edu.westga.jetnoisereporter.Model;
+
+
+public class User {
+    private String name;
+    private String address;
+    private String city;
+    private String zipcode;
+    private String phone;
+    private String callerCode;
+
+    public User(String name, String address, String city, String zipcode, String phone, String callerCode) {
+
+    }
+}
