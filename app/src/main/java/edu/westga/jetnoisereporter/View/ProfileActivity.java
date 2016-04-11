88181335c14747919e68a320ce2fd9c2af36864d
Kenji Okamoto <kenjio@users.noreply.github.com>
2016-04-11 13:22:49 -0700

    Implement the profile activity layout and button handler

--- app/src/main/java/edu/westga/jetnoisereporter/View/ProfileActivity.java
+++ app/src/main/java/edu/westga/jetnoisereporter/View/ProfileActivity.java
@@ -6,25 +6,45 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import edu.westga.jetnoisereporter.Controller.JetNoiseAppController;
 import edu.westga.jetnoisereporter.R;
 
 public class ProfileActivity extends AppCompatActivity {
+    private JetNoiseAppController controller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        this.controller = new JetNoiseAppController(this);
     }
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+    public void onSubmitButtonClick(View v) {
+        String name = ((EditText) findViewById(R.id.nameEditText)).getText().toString().trim();
+        String  address = ((EditText) findViewById(R.id.streetEditText)).getText().toString().trim();
+        String  city = ((EditText) findViewById(R.id.cityEditText)).getText().toString().trim();
+        String  zipcode = ((EditText) findViewById(R.id.zipcodeEditText)).getText().toString().trim();
+        String  phone = ((EditText) findViewById(R.id.phoneEditText)).getText().toString().trim();
+        if (name.equals("") || address.equals("") || city.equals("") || zipcode.equals("") || phone.equals("")) {
+            Toast.makeText(ProfileActivity.this, "All fields must be entered", Toast.LENGTH_SHORT).show();
+        } else {
+            this.controller.updateUser(name, address, city, zipcode, phone, "code");
+        }
+    }
+
+
 }
