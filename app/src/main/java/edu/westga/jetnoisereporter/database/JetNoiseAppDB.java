ea3d34ef8b49813dea0c7f4da51ad4a25838fde5
Kenji Okamoto <kenjio@users.noreply.github.com>
2016-04-11 10:26:35 -0700

    Database methods to create/update a user and query a user

--- app/src/main/java/edu/westga/jetnoisereporter/database/JetNoiseAppDB.java
+++ app/src/main/java/edu/westga/jetnoisereporter/database/JetNoiseAppDB.java
@@ -1,6 +1,8 @@
 package edu.westga.jetnoisereporter.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -22,8 +24,8 @@ public class JetNoiseAppDB extends SQLiteOpenHelper {
     public static final String COLUMN_REPORT_ID = "reportID";
     public static final String COLUMN_REPORT_DATE = "reportDate";
 
-    public JetNoiseAppDB(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+    public JetNoiseAppDB(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
@@ -38,13 +40,6 @@ public class JetNoiseAppDB extends SQLiteOpenHelper {
                 + COLUMN_PHONE + " TEXT"
                 + ");";
         db.execSQL(CREATE_USERS_TABLE);
-
-        String CREATE_PRODUCTS_TABLE = "CREATE TABLE " + TABLE_REPORTS + "("
-                + COLUMN_REPORT_ID + " INTEGER PRIMARY KEY,"
-                + COLUMN_REPORT_DATE + " DATETIME,"
-                + COLUMN_USER_ID + " INTEGER REFERENCES " + TABLE_USERS
-                + ")";
-        db.execSQL(CREATE_PRODUCTS_TABLE);
     }
 
     @Override
@@ -52,6 +47,43 @@ public class JetNoiseAppDB extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_REPORTS);
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERS);
         onCreate(db);
+    }
+
+    public long updateUser(String name, String address, String city, String zipcode,
+                           String phone, String callerCode) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        // This app is only for a single user so delete all rows and re-create the single user
+        String deleteQuery = "DELETE FROM " + TABLE_USERS;
+        db.execSQL(deleteQuery);
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAME, name);
+        values.put(COLUMN_PHONE, phone);
+        values.put(COLUMN_ADDRESS, address);
+        values.put(COLUMN_CITY, city);
+        values.put(COLUMN_ZIP, zipcode);
+        values.put(COLUMN_CALLER_CODE, callerCode);
+        //values.put(COLUMN_REPORT_DATE,"2013-10-07 08:23:19.120");
+        // Insert the new row, returning the primary key value of the new row
+        long newRowId;
+        newRowId = db.insert(TABLE_USERS, null, values);
+        db.close();
+        return  newRowId;
+    }
+
+    public String lookupUser() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = "SELECT " + COLUMN_NAME + " FROM " + TABLE_USERS;
+        Cursor cursor = db.rawQuery(query, null);
+        String result = "Not Found";
+        if (cursor.moveToFirst()) {
+            cursor.moveToFirst();
+            result = cursor.getString(0);
+            cursor.close();
+        }
+        db.close();
+        return result;
     }
 
 }
